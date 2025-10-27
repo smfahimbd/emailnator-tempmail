4f4de25b5820ce765598b5c1db92244f4c5826ba
S M Fahim <158431295+smfahimcp@users.noreply.github.com>
2025-10-27 21:45:47 +0600

    Add files via upload

--- utils/spoof-head.js
+++ utils/spoof-head.js
@@ -8,7 +8,10 @@ const SpoofHead = (extra = {}) => {
     "client-ip": ip,
     "x-client-ip": ip,
     "x-cluster-client-ip": ip,
-    "x-original-forwarded-for": ip
+    "x-original-forwarded-for": ip,
+    "x-forwarded-proto": "https",
+    "x-forwarded-host": "www.emailnator.com",
+    "x-forwarded-port": "443"
   };
   return {
     ...genericHeaders,
